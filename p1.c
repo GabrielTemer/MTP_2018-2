976f949f1ff5b5c1b8b980d6234105501378b4f2
GabrielTemer <42917958+GabrielTemer@users.noreply.github.com>
2018-09-26 09:13:10 -0300

    Update p1.c

--- p1.c
+++ p1.c
@@ -5,42 +5,46 @@
 //p1.c
 
 #include <stdio.h>
-
 int main()
 {
-   int estado=0, i=0;
-   char bits[256];
-   scanf("%s", &bits);
-   while(bits[i]!='\0'){
-    if(estado == 0 && bits[i]== '1'){
-        estado = 1;
-        i++;
-    }
-    if(estado == 0 && bits[i]== '0'){
-        estado = 0;
-         i++;
-    }
-    if(estado == 1 && bits[i]== '1'){
-        estado = 0;
-         i++;
-    }
-    if(estado == 1 && bits[i]== '0'){
-        estado = 2;
-         i++;
-    }
-    if(estado == 2 && bits[i]== '0'){
-        estado = 1;
-         i++;
-    }
-    if(estado == 2 && bits[i]== '1'){
-        estado = 2;
-         i++;
-    }
-
-   }
-   if(estado==0){
-   printf("%s eh multiplo de 3", bits);
-}
-else {
-    printf("%s nao eh multiplo de 3", bits);
+	char n[256];
+	int estado=0, i=0;
+	scanf("%s",n);
+	while(n[i]!='\0')
+	{
+		if(estado==0 && n[i]=='1')
+		{
+			estado = 1;
+			i++;
+		}
+		if(estado==0 && n[i]=='0')
+		{
+			estado = 0;
+			i++;
+		}
+		if(estado==1 && n[i]=='1')
+		{
+			estado = 0;
+			i++;
+		}
+		if(estado==1 && n[i]=='0')
+		{
+			estado = 2;
+			i++;
+		}
+		if(estado ==2 && n[i]=='0')
+		{
+			estado = 1;
+			i++;
+		}
+		if(estado ==2 && n[i]=='1')
+		{
+			estado = 2;
+			i++;
+		}
+	}
+	printf("%s ", n);
+	if(estado!=0)
+	printf("nao ");
+	printf("e multiplo de 3");
 }
